66985626da65b489ec3da3eb3872a024e507bd5d
Sofia Saraiva <112433998+Sofia-Saraiva@users.noreply.github.com>
2023-06-13 19:34:28 -0300

    Update autenticacao.py

--- autenticacao.py
+++ autenticacao.py
@@ -48,6 +48,11 @@ def login():
     else:
         return render_template('login.html')
 
+@app.route("/logout")
+def logout():
+
+    session.clear()
+    return redirect("/")
 
 
 @app.route("/register", methods=["GET", "POST"])
