2d1a7b32570e5514dde1ac81a48eb4e0ce30056f
Mateus Roberto Oliveira <mateusoliveira110085@gmail.com>
2023-06-14 13:46:17 -0300

    Alteração Main V7

--- main-v2.py
+++ main-v2.py
@@ -1,20 +1,36 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import login_required
+from flask_login import login_required, LoginManager, UserMixin, login_user, logout_user
 import sqlite3
-
+import os
 
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["SECRET_KEY"] = 'mangueHeat'
 
-con = sqlite3.connect("users.db")
-cursor = con.cursor()
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL);')
-cursor.execute('CREATE UNIQUE INDEX IF NOT EXISTS username ON users (username);')
+class User(UserMixin):
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+
+@login_manager.user_loader
+def load_user(user_id):
+    con = sqlite3.connect("users.db")
+    cursor = con.cursor()
+    colunas = cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+    coluna = colunas.fetchone()
+
+    if coluna:
+        user = User(id=coluna[0], username=coluna[1])
+        return user
+
+    return None
 
 @app.route('/')
 def index():
@@ -25,6 +41,8 @@ def login():
     session.clear()
 
     if request.method == 'POST':
+        con = sqlite3.connect("users.db")
+        cursor = con.cursor()
         username = request.form.get("username")
         senha = request.form.get('senha')
 
@@ -33,27 +51,31 @@ def login():
         elif not senha:
             return render_template("login.html", info='Deve fornecer senha.')
 
-        colunas = cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-        coluna = colunas.fetchone()
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        coluna = cursor.fetchone()
 
         if not coluna or not check_password_hash(coluna[2], senha):
             return render_template("login.html", info='Credenciais inválidas')
 
-        session["user_id"] = coluna[0]
+        user = User(id=coluna[0], username=coluna[1])
+        login_user(user)
 
-        return redirect("/")
+        return redirect(url_for("index"))
 
     else:
         return render_template('login.html')
 
 @app.route("/logout")
+@login_required
 def logout():
-    session.clear()
-    return redirect("/")
+    logout_user()
+    return redirect(url_for("index"))
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        con = sqlite3.connect("users.db")
+        cursor = con.cursor()
         username = request.form.get("username")
         senha = request.form.get("senha")
         confirmation = request.form.get("confirmation")
@@ -68,13 +90,20 @@ def register():
         if senha != confirmation:
             return render_template("register.html", info="Senhas estão diferentes.")
 
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        coluna = cursor.fetchone()
+
+        if coluna is not None:
+            return render_template("register.html", info="Nome de usuário já existe.")
+
         try:
             hash = generate_password_hash(senha)
             cursor.execute("INSERT INTO users (username, hash) VALUES (?, ?)", (username, hash))
             con.commit()
-            return redirect("/")
+            return redirect(url_for("login"))
         except:
-            return render_template("register.html", info="Nome de usuário já existe.")
+            return render_template("register.html", info="Erro ao criar usuário.")
+
     else:
         return render_template("register.html")
 
@@ -87,7 +116,9 @@ def biblioteca():
 @login_required
 def upload():
     if request.method == 'POST' and 'audio' in request.files:
+
         return redirect(url_for('biblioteca'))
+
     return render_template('upload.html')
 
 if __name__ == '__main__':
